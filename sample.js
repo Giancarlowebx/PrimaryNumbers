20ba136a3f93b122d9d399c2924d8c614c8a861a
Giancarlowebx <giancarlo@web123.com>
2021-06-20 15:18:29 +0800

    Edited

--- sample.js
+++ sample.js
@@ -3,13 +3,13 @@ function submit_Num() {
     let arr = [];
     let x = document.getElementById("input_num").value;
     let z = 0;
-
+    let total = 0;
+    
     for(i = 2; i <= x; i++){    
         let count0 = 0;
 
         for(w = 2; w <= x; w++){
             z = i % w;
-
             if(z == 0){
                 count0 += 1;
             }
@@ -19,6 +19,13 @@ function submit_Num() {
         }
     }
 
+
+    
+    for(a = 0; a < arr.length; a++){
+        total = total + arr[a];     
+    }
+    
+    console.log(total);
     document.getElementById("result_Container").innerHTML = arr;
     document.getElementById("length_Count").innerHTML = arr.length;
 }
